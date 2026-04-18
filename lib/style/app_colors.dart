4683626b3f6f0c54b0d351ff39889d618018d789
Amanda Lemos Ribas <amandaribas@id.uff.br>
2026-04-17 21:38:50 -0300

    AppColors
    Padornização de cores que serão utilizadas no Projeto

---
+++ lib/style/app_colors.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // Cores Principais e Cenário
+  static const Color buttomVerde = Color(0xFF0CA663);
+  static const Color bgVerde = Color(0xFF01844B); //verde principal
+  static const Color preto = Color(0xFF000000);
+  static const Color branco = Color(0xFFFFFFFF);
+  static const Color bgCinza = Color(0xFFD9D9D9);
+  static const Color borderCinza = Color(0xFF4A4A4A);
+  
+  // Rank
+  static const Color ouro = Color(0xFFFABA42);
+  static const Color prata = Color(0xFF919191);
+  static const Color bronze = Color(0xFF7D4029);
+
+  //Denunciar usuário
+  static const Color vermelho = Color(0xFFA12E2E);
+
+  // Categorias
+  static const Color diarioRosa = Color(0xFFF884F6);
+  static const Color semanalRoxo = Color(0xFFB884F8);
+  static const Color mensalAmarelo = Color(0xFFFABA42); // Reutilizando a cor ouro
+
+  // Estatísticas
+  static const Color statCo2 = Color(0xFF01844B);
+  static const Color staAgua = Color(0xFF15A1E6);
+  static const Color statReciclado = Color(0xFF701DD7);
+  static const Color statTransporte = Color(0xFFE47507);
+
+
+}
